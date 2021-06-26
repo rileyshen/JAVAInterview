307784dc5c2671d1cefbd670ccd331d41f4b82b1
RileyCode <ripple.shen31@gmail.com>
2021-06-26 20:47:57 +0800

    volatileNEatomic

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-java interview preprare
+java interview prepare
 
 ***java volatile***
  + [before add volatile keyword]()
+ +  [add volatile keyword]()
+ +  [java volatile ≠ atomic]()
--- src/main/java/com/exa/study/thread/VolatileDemo.java
+++ src/main/java/com/exa/study/thread/VolatileDemo.java
@@ -9,17 +9,50 @@ class MyData {
     public void addTo60() {
         this.number = 60;
     }
+
+    public void addPlusPlus() {
+        number++;
+    }
 }
 
 /**
  *1.prove java volatile visibility
  * 1.1 assume int number = 0;  add volatile keyword before changing value of number
+ * 1.2 add volatile keyword to ensure visibility
+ *
+ *2.This update of a volatile variable is non-atomic
+ *  2.1 atomic :
+ *      An atomic operation is one that cannot be interrupted by the thread
+ *      method was executed successfully or failure
+ *  2.2 cannot ≠ atomic :
  */
 
 public class VolatileDemo {
 
     public static void main(String[] args) {
 
+        MyData myData = new MyData();
+
+        for (int i = 0; i < 20; i++) {
+
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    myData.addPlusPlus();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        // after Calculating(20 Threads), use main thread to get the finial result
+        while (Thread.activeCount() > 2) { // main thread and GC thread at backstage, wait util calculating
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t final number value: " + myData.number);
+
+    }
+
+    // volatile guarantees visibility of changes to variables across threads
+    public static void seeByVolatile() {
         MyData myData = new MyData();
 
         new Thread(() -> {
